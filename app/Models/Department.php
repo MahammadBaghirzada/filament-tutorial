b9d7aaa6cebb7a18e066b2422d5cd45cc2ffc560
Mahammad <mbagirzade.01@gmail.com>
2023-09-21 11:42:40 +0400

    Create Employees

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
@@ -12,4 +13,9 @@ class Department extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
